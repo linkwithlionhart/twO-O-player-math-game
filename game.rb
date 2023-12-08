6d54fd36eeb587e2b4cced5a96b1ef5958a08e6e
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-08 00:44:58 -0800

    debug and refactor game script

--- game.rb
+++ game.rb
@@ -12,6 +12,14 @@ class Game
       question = Question.new
       puts "#{@current_player.name}: #{question.ask}"
       user_answer = get.chomp.to_i
+      if question.correct?(user_answer)
+        puts "YES! You are correct."
+      else
+        puts "Seriously? No!"
+        @current_player.lost_life
+      end
+      show_lives
+      switch_player
     end
     game_over
   end
@@ -23,6 +31,14 @@ class Game
   def game_over
     puts "----- GAME OVER -----"
     puts "Good bye!"
+  end
+
+  def show_lives
+    puts "P1 #{player_1.lives}/#{player_1.total_lives} vs P2 #{player_2.lives}/#{player_2.total_lives}"
+  end
+
+  def switch_player
+    @current_player = (@current_player == @player_1) ? @player_2 : @player_1
   end
 
 end
